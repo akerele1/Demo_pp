f239b3b10532d809f4cac633d6b983f29645a3e9
Victor Akerele <akerelev@yahoo.com>
2026-04-24 23:07:48 +0100

    Auto save: .\pages\blossom_page.py

--- pages/blossom_page.py
+++ pages/blossom_page.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Page, expect
 
 class blossomPage:
+    
     def __init__(self, page: Page):
         self.page = page
         self.url = 'https://theprosperingplace.co.uk/product/blossom/'
@@ -16,4 +17,3 @@ class blossomPage:
         services = self.page.locator("#menu-item-1399").get_by_role("link", name="Services")
         expect(services).to_be_visible()
         services.click()
-        
