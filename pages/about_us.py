36dbde9f166e7bdf83fdfa028e5b107ef5a9a647
Victor Akerele <akerelev@yahoo.com>
2026-04-25 21:51:58 +0100

    Auto save: .\.venv\Lib\site-packages\playwright\driver\package\lib\server\chromium\crCoverage.js

--- pages/about_us.py
+++ pages/about_us.py
@@ -11,7 +11,7 @@ class aboutUs():
         self.page.goto(self.url)
 
     def open_about_us_page(self):
-        about_us = self.page.
+        about_us = self.page.get
 
     def get_aboutus_title(self):
         about_title = self.page.title()
