dc65a48a001218a44bd232f1a09d604e8c4a89c7
Victor Akerele <akerelev@yahoo.com>
2026-04-25 22:12:26 +0100

    Auto save: .\tests\test_about_us.py

--- tests/test_about_us.py
+++ tests/test_about_us.py
@@ -6,5 +6,5 @@ def test_about_us(page: Page):
     about = aboutUs(page)
     about.open_url()
 
-def test_go_to_about_us(page: Page)
+def test_go_to_about_us(page: Page):
     open_about_us = aboutUs
