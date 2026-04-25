c151d175309741c791445a7ab568c742aa7ba53f
Victor Akerele <akerelev@yahoo.com>
2026-04-25 22:05:42 +0100

    Auto save: .\pages\about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -7,7 +7,7 @@ class aboutUs():
         self = self.page
         self.url = "https://theprosperingplace.co.uk/"
 
-    def load(self):
+    def (self):
         self.page.goto(self.url)
 
     def open_about_us_page(self):
