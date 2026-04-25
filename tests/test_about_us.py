f66a4313b02d11ffa691fdfe84f49bf71ad231e3
Victor Akerele <akerelev@yahoo.com>
2026-04-25 21:59:31 +0100

    Auto save: .\.venv\Lib\site-packages\playwright\_impl\_file_chooser.py

--- tests/test_about_us.py
+++ tests/test_about_us.py
@@ -3,7 +3,6 @@ from pages.about_us import aboutUs
 from playwright.sync_api import Page
 
 def test_about_us(page: Page):
-    about =aboutUs(page)
+    about = aboutUs(page)
     #open url
     about.open_url()
-    
